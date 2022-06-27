1770604e08ce7ecd92fb2534211f9f0d986a6ba3
liptao <limpa0099@gmail.com>
2022-06-26 20:01:40 -0400

    Complete repeatString func

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n < 0) {
+        return 'ERROR'
+    }
+    let out = ''
+    for (i=0; i<n; i++) {
+        out += string;
+    }
+    return out;
 };
 
 // Do not edit below this line
