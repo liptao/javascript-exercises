d2d451d584254211c785d2b7b87463f6907c2bcf
liptao <limpa0099@gmail.com>
2022-06-26 20:14:07 -0400

    Add reverseString func

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    const stringAsArray = string.split('');
+    stringAsArray.reverse();
+    return stringAsArray.join('');
+}
 
 // Do not edit below this line
 module.exports = reverseString;
