3b4e0e1402bd57cec614b27e9d6976a89207e1a1
liptao <limpa0099@gmail.com>
2022-06-27 23:54:39 -0400

    Complete sumAll func

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if ((first < 0 || second < 0) || (!Number.isInteger(first) || !Number.isInteger(second))) {
+        return 'ERROR'
+    }
+    const max = Math.max(first, second);
+    const min = Math.min(first, second);
+    let sum = 0;
+    
+    for (let i=min; i <= max; i++) {
+        sum += i
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
