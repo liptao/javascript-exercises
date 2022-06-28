656757b28622f9f6e4118d45f626b1b3aca7636a
liptao <limpa0099@gmail.com>
2022-06-28 00:16:06 -0400

    Fixed func semicolon

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(fTemp) {
   // [°C] = ([°F] − 32) × 5⁄9
-  return Math.round((fTemp - 32) * 5/9 * 10) / 10
+  return Math.round((fTemp - 32) * 5/9 * 10) / 10;
 };
 
 const ctof = function(cTemp) {
   // [°F] = [°C] × 9⁄5 + 32
-  return Math.round((cTemp * 9/5 + 32) * 10) / 10
+  return Math.round((cTemp * 9/5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
